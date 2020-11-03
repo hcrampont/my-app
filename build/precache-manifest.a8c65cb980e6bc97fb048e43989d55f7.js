7a272e65be29839e983a5a1433b0af0167c82438
Hugo Crampont <hugo.crampont@altares.com>
2020-11-03 14:12:36 +0100

    add build folder

---
+++ build/precache-manifest.a8c65cb980e6bc97fb048e43989d55f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1403017a5065f4cbc6569e80365ab54c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7389ddaf623f175edd40",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01bb30c1d71e9eb5be6d",
+    "url": "/static/js/2.80ecc2e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80ecc2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7389ddaf623f175edd40",
+    "url": "/static/js/main.5ee728be.chunk.js"
+  },
+  {
+    "revision": "50c4c0b32d7982b17e9d",
+    "url": "/static/js/runtime-main.5c7d1aba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
